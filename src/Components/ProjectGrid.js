2198afe9fb2b8d5d44b896c9623d2249c0374325
Andres Leal <pixele9@gmail.com>
2020-07-21 23:39:45 -0500

    working on project page details

--- src/Components/ProjectGrid.js
+++ src/Components/ProjectGrid.js
@@ -1,9 +1,15 @@
 import React from 'react';
 // sm:bg-green-300 md:bg-green-700
+
+import Eye from '../Assets/Imgs/EyeIcon.png';
+
 export default function ProjectGrid(props) {
 	return (
-		<div className="flex justify-center flex-col items-center ">
-			<img src={ props.image } alt="PLE project" className="rounded h-56 sm:h-64" />
+		<div className="flex justify-center flex-col items-center">
+			<div className="relative project-card flex items-center justify-center">
+				<img src={ props.image } alt="project view" className="rounded h-56 sm:h-64 hover:opacity-50 cursor-pointer" />
+				<img src={ Eye } alt="view project icon" className="eye-icon absolute hover:block h-7 w-10" />
+			</div>
 			<p className="text-white mt-3">{ props.project_title }</p>
 		</div>
 	);
---
+++ src/Components/ProjectModal.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function ProjectModal(props) {
+	return(
+		<div className="absolute flex flex-col sm:flex-row rounded-lg w-8/12 bg-white ">
+			<div className="left flex flex-col">
+				<img src={ props.image1 } alt="project img" className="rounded mx-5 mt-5"/>
+				<img src={ props.image2 } alt="project img" className="rounded m-5 mb-5"/>
+			</div>
+			<div className="right p-10">
+				<h1 className="font-bold text-3xl montserrat">{ props.projectName }</h1>
+				<h3 className="text-xl montserrat font-bold mt-6">Project Description:</h3>
+				<p>{ props.description }</p>
+
+				<h3 className="text-xl montserrat font-bold mt-6">What I learned:</h3>
+				<p>{ props.learned }</p>
+			</div>
+		</div>
+	);
+}
