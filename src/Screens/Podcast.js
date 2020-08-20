fe60a3d53226718612c02d9dd6107d4c2afdcd63
Andres Leal <pixele9@gmail.com>
2020-08-19 19:49:19 -0500

    *added* Google Analytics

--- src/Screens/Podcast.js
+++ src/Screens/Podcast.js
@@ -4,6 +4,7 @@ import podcastParser from 'node-podcast-parser';
 import request from 'request';
 
 import PodcastCard from '../Components/PodcastCard';
+import { useEffect } from 'react';
 
 // https://developer.spotify.com/documentation/web-api/reference/shows/get-shows-episodes/
 // parse all spotify episodes and embed them here
@@ -11,7 +12,8 @@ import PodcastCard from '../Components/PodcastCard';
 export default function Podcast() {
 	const [podcastData, setPodcastData] = useState({})
 
-	// fetch("https://anchor.fm/s/2c51819c/podcast/rss")
+	// async function getRSS() {
+	// 	fetch("https://anchor.fm/s/2c51819c/podcast/rss")
 	// 	.then(response => {
 	// 		return response
 	// 	})
@@ -29,34 +31,98 @@ export default function Podcast() {
 	// 		setPodcastData(data.episodes)
 			
 	// 	})
+	// }
+	let response = {}
+	let podcastInfo = {}
 
-	request("https://anchor.fm/s/2c51819c/podcast/rss", (err, res, data) => {
-		if (err) {
-			console.error("Network error", err);
-			return;
+	useEffect(() => {
+		async function fetchData() {
+			await request("https://anchor.fm/s/2c51819c/podcast/rss", (err, res, data) => {
+				if (err) {
+					console.error("Network error", err);
+					return;
+				}
+	
+				podcastParser(data, (err, data) => {
+					if (err) {
+						console.error("Parsing error", err);
+						return;
+					}
+	
+					setPodcastData(data.episodes)
+					console.log(podcastData)
+				})
+				// setPodcastData(data.episodes)
+			})
 		}
+		fetchData()
+	}, [])
 
-		podcastParser(data, (err, data) => {
+
+	async function getRSS() {
+		let response = {}
+		let res = await request("https://anchor.fm/s/2c51819c/podcast/rss", (err, res, data) => {
 			if (err) {
-				console.error("Parsing error", err);
+				console.error("Network error", err);
 				return;
 			}
 
-			// console.log(data.episodes)
+			podcastParser(data, (err, data) => {
+				if (err) {
+					console.error("Parsing error", err);
+					return;
+				}
+
+				if (data) {
+					// setPodcastData(data.episodes)
+					podcastInfo = data.episodes
+					response = data.episodes
+					return response
+				}
+				// console.log(data.episodes)
+			})
+			// setPodcastData(data.episodes)
 		})
-		setPodcastData(data.episodes)
-	})
+		console.log(res.json())
+		return res.json()
+	}
+	// console.log(getRSS())
+
+	async function getFeed() {
+		try {
+			const resFeed = await fetch("https://anchor.fm/s/2c51819c/podcast/rss")
+			const feed = resFeed
+
+			podcastParser(feed, (err, data) => {
+				if (err) {
+					console.log("Parsing error: ", err)
+				}
+
+				// if (data) {
+				// 	return data.episodes
+				// }
+				console.log(data)
+			})
+		} catch (error) {
+			console.log("ERROR: ", error)
+		}
+	}
 	return (
+		// getRSS() ? ( <h1>{podcastInfo[0]}</h1> ) : null
 		<div className="w-64 p-16">
+			sdfsdfsd
 			{
-				console.log(podcastData)
-				// podcastData != {} ? (
-				// 	podcastData.episodes.map(data => (
+				// console.log(podcastInfo)
+				// podcastInfo.map(data => (
+				// 	<PodcastCard />
+				// ))
+				// podcastInfo != {} ? (
+				// 	podcastInfo.episodes.map(data => (
 				// 		<PodcastCard />
 				// 	))
 				// ) : null 
 			}
-			<iframe title="Podcast Feed" src="https://open.spotify.com/embed-podcast/episode/2XpH2HhoFHkCe9HNiXaeJx" width="100%" height="232" frameBorder="0" allowtransparency="true" allow="encrypted-media" className="m-16"></iframe>
+			{/* <iframe title="Podcast Feed" src="https://open.spotify.com/embed-podcast/episode/2XpH2HhoFHkCe9HNiXaeJx" width="100%" height="232" frameBorder="0" allowtransparency="true" allow="encrypted-media" className="m-16"></iframe> */}
 		</div>
 	)
 }
